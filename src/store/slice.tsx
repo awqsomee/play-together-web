4c0d7fff184359a35eb82e855a6ee261203d4b77
awqsomee <awqsome@bk.ru>
2022-12-21 06:51:10 +0300

    store

---
+++ src/store/slice.tsx
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit'
+const slice = createSlice({
+  name: 'toolkit',
+  initialState: {},
+  reducers: {},
+})
+
+export default slice.reducer
+// export const {}
